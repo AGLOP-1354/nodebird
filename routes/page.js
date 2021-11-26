8de2a88c5eb9e5acc7e33444f2650051ec982e89
jake <jake@jober.io>
2021-11-26 15:46:01 +0900

    passport 로그인 기능 구현

--- routes/page.js
+++ routes/page.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.locals.user = null;
+  res.locals.user = req.user;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
   res.locals.followerIdList = [];
   next();
 });
 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
-router.get('/join', (req, res) => {
+router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
 
@@ -27,3 +28,4 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
